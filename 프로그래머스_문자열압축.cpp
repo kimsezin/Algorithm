13f480a0f996eaae0078e8f9cb7d77b93b2d72d4
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-07-20 13:22:56 +0900

    algorithm

---
+++ 프로그래머스_문자열압축.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string s) {
+	int length = s.size();
+	int answer = length;
+
+	for (int i = 1; i <= length / 2; i++)
+	{
+		string result = "";
+
+		string temp = s.substr(0, i);
+		int cnt = 1;
+		for (int j = i; j <= length; j += i)
+		{
+			if (temp == s.substr(j, i))
+			{
+				cnt++;
+			}
+			else
+			{
+				if (cnt == 1)
+				{
+					result += temp;
+				}
+				else
+				{
+					result += (to_string(cnt) + temp);
+				}
+				temp = s.substr(j, i);
+				cnt = 1;
+			}
+		}
+		if (length % i != 0)
+		{
+			result += s.substr((length / i) * i);
+		}
+
+		if (answer > result.size())
+		{
+			answer = result.size();
+		}
+	}
+	return answer;
+}
+
+int main()
+{
+	string s = "abcabcabcdeddedded";
+	solution(s);
+}

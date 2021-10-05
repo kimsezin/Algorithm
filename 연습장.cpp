4a1f409d9723880cb03645ac793866622d39aeb5
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-10-05 18:27:15 +0900

    algorithm

--- 연습장.cpp
+++ 연습장.cpp
@@ -1,53 +1,77 @@
-#include <string>
-#include <vector>
-#include<map>
-#include <iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<iostream>
 using namespace std;
 
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
 
+int N, M;
 
-int solution(string s) {
-    int answer = 0;
-    map<int, string> map;
+vector<int> result;
+vector<vector<int>> map;
+struct info
+{
+	int x, y, cnt;
+};
+int BFS(int x, int y, int cnt, vector<vector<bool>> visited)
+{
+	queue<info> q;
+	q.push({ 0,0,1 });
+	visited[0][0] = 1;
 
-    map[0] = "zero";
-    map[1] = "one";
-    map[2] = "two";
-    map[3] = "three";
-    map[4] = "four";
-    map[5] = "five";
-    map[6] = "six";
-    map[7] = "seven";
-    map[8] = "eight";
-    map[9] = "nine";
-    string temp = "";
+	while (!q.empty())
+	{
+		int nx = q.front().x;
+		int ny = q.front().y;
+		int n_cnt = q.front().cnt;
 
-    string check = "";
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            temp += s[i];
-        }
-        else
-        {
-            check += s[i];
-            for (int i = 0; i <= 9; i++)
-            {
-                if (map[i] == check)
-                {
-                    check = "";
-                    temp += map[i];
-                }
-            }
+		q.pop();
 
-        }
-    }
-    answer = stoi(temp);
-    return answer;
+		if (nx == N - 1 && ny == N - 1)
+		{
+			return n_cnt;
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			int Tx = nx + dx[i];
+			int Ty = ny + dy[i];
+			if (Tx >= 0 && Ty >= 0 && Tx < N && Ty < M)
+			{
+				if (visited[Tx][Ty] == 0 && map[Tx][Ty] == 1)
+				{
+					visited[Tx][Ty] = 1;
+					q.push({ Tx, Ty, n_cnt + 1 });
+				}
+
+			}
+		}
+	}
+	return -1;
+}
+
+int solution(vector<vector<int>> maps)
+{
+	int answer = 0;
+	map = maps;
+
+	int x = 0, y = 0, cnt = 1;
+
+	N = maps.size();
+	M = maps[0].size();
+	vector<vector<bool>> visited(N, vector<bool>(M, false));
+
+	return BFS(0, 0, 1, visited);
 }
 
 int main()
 {
-    solution("one4seveneight");
+	solution({ {1, 0, 1, 1, 1
+}, { 1, 0, 1, 0, 1
+}, { 1, 0, 1, 1, 1
+}, { 1, 1, 1, 0, 1
+}, { 0, 0, 0, 0, 1
+} });
 }
